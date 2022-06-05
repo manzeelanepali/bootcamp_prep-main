898cec66e2bdc25abc1ceeae5ec0d6b401e9c6aa
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-05 14:21:05 +0545

    zero dark problems

--- workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
+++ workshops/03-coercion-and-truthiness/05-zero-dark-thirty/zero-dark-thirty.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function zeroDarkThirty(num) {
+  if (num === 0) {
+    return NaN;
+  }
+  debugger;
+
+  let finalstring = "";
+  let stringnum = String(num);
+  for (let i = 0; i < stringnum.length; i++) {
+    if (stringnum[i] !== "0") {
+      finalstring += stringnum[i];
+    }
+  }
+  return Number(finalstring);
+}
+console.log(zeroDarkThirty(1000));
