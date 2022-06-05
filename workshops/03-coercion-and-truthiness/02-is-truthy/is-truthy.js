33262127c6e6a0dec86252d6190ff41fe064f425
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-05 12:02:30 +0545

    coercion problem number 2

--- workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
+++ workshops/03-coercion-and-truthiness/02-is-truthy/is-truthy.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+function isTruthy(x) {
+  if (x === "") {
+    return "The empty string is falsey (the only falsey string)";
+  }
+  if (x === 0) {
+    return "The number 0 is falsey (the only falsey number)";
+  }
+  if (x === undefined) {
+    return "undefined is falsey";
+  }
+  if (x === null) {
+    return "The null value is falsey";
+  }
+  if (x === false) {
+    return "The boolean value false is falsey";
+  } else {
+    return true;
+  }
+}
