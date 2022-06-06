5614e60f80365e5e2f63ba400db38112a97e0ac3
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-06 12:09:07 +0545

    incremenrtal change

--- workshops/04-scope/02-incremental-change/incremental-change.js
+++ workshops/04-scope/02-incremental-change/incremental-change.js
@@ -1,3 +1,12 @@
 let counter = 0;
+function incrementalChange(number) {
+  if (number > 0) {
+    for (i = 0; i < number; i++) counter++;
+    return counter;
+  } else {
+    for (i = 0; i > number; i--) counter--;
+    return counter;
+  }
+}
 
 // YOUR CODE BELOW
