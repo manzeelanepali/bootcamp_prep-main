987ea9047b593c6ff6d72900920a47b48549e402
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-08 13:47:16 +0545

    third problem of array

--- workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
+++ workshops/05-arrays-i/03-my-last-index-of/my-last-index-of.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+function myLastIndexOf(array, searchValue, startIdx = 0) {
+  let counter = 0;
+
+  for (let i = startIdx; i < array.length; i++) {
+    if (searchValue === array[i]) {
+      counter = i - startIdx;
+    }
+  }
+  if (counter === 0) {
+    return -1;
+  } else {
+    return counter;
+  }
+}
+
+console.log(myLastIndexOf([0, 10, 20, 10, 0], 10, 2));
