bca0de21683ab3baa3956f6beccbf0d60fdd8610
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-08 16:02:15 +0545

    reverse method using unsft in array

--- workshops/05-arrays-i/04-my-reverse/my-reverse.js
+++ workshops/05-arrays-i/04-my-reverse/my-reverse.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+function myReverse(array) {
+  let FinalArray = [];
+
+  for (let i = 0; i < array.length; i++) {
+    FinalArray.unshift(array[i]);
+  }
+  return FinalArray;
+}
+console.log(myReverse([1, 2, 3]));
