67e986b1df27641e7bcd7e757b2d4fc127ff9e67
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-08 11:03:50 +0545

    array question num 2

--- workshops/05-arrays-i/02-my-includes/my-includes.js
+++ workshops/05-arrays-i/02-my-includes/my-includes.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function myIncludes(array, searchValue) {
+  let currentValue = [];
+  {
+    for (let i = 0; i < array.length; i++) currentValue = array[i];
+
+    if (currentValue === searchValue) {
+      return true;
+    } else false;
+  }
+  console.log(array());
+}
