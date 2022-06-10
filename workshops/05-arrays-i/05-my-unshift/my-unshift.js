87fd74ce850df5cd97773bf1ebfc2c39083bc703
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-10 13:04:15 +0545

    object -method problem 1

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,1 +1,10 @@
 // YOUR CODE BELOW
+function unShift(array, value) {
+  let finalVal = [value];
+  for (let i = 0; i < array.length; i++) {
+    finalVal.unshift(array[i]);
+  }
+
+  return finalval;
+}
+console.log(unshift([a, b, c], r));
--- workshops/07-objects/01-last-friday-night/last-friday-night.js
+++ workshops/07-objects/01-last-friday-night/last-friday-night.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+function lastFridayNight(transactions) {
+  let totalSum = 0; //returns the total sum
+  for (let i = 0; i < transactions.length; i++) {
+    let currentValue = transactions[i];
+    let currentAmount = transactions[i].amount;
+    totalSum += currentAmount;
+  }
+  return totalSum;
+}
