09e848f0427c74c39e5811a9e5c82d437d22b117
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-11 17:14:32 +0545

    my unshift

--- workshops/05-arrays-i/05-my-unshift/my-unshift.js
+++ workshops/05-arrays-i/05-my-unshift/my-unshift.js
@@ -1,10 +1,10 @@
 // YOUR CODE BELOW
-function unShift(array, value) {
-  let finalVal = [value];
+// unShift([1, 2, 3], 0); / => [0, 1, 2, 3];
+function myUnshift(array, value) {
+  let result = [value];
   for (let i = 0; i < array.length; i++) {
-    finalVal.unshift(array[i]);
+    let currValue = array[i];
+    result.push(currValue);
   }
-
-  return finalval;
+  return result;
 }
-console.log(unshift([a, b, c], r));
