73244b2878463db6ece6e4950ade5b8abcfb1299
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-12 19:02:35 +0545

    even and offer

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+// evenAndOdd([1, 2, [3, 4, 5, 6]); // => [[ 2 ,4, 6 ], [ 1, 3, 5 ]]
+function evenAndOdd(Array) {
+  let result = [];
+  for (let i = 0; i < Array.length; i++) {
+    let currValue = Array[i];
+    if (currValue % 2 === 1) {
+      result.push(currValue);
+    }
+  }
+  return result;
+}
