7cf7d982a27264f22e6fb017e2989e551ef1bfdc
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-12 21:36:50 +0545

    even odd array problem number 2

--- workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
+++ workshops/06-arrays-ii/01-even-and-odd/even-and-odd.js
@@ -1,12 +1,16 @@
 // YOUR CODE BELOW
 // evenAndOdd([1, 2, [3, 4, 5, 6]); // => [[ 2 ,4, 6 ], [ 1, 3, 5 ]]
 function evenAndOdd(Array) {
-  let result = [];
+  let evenresult = [];
+  let oddresult = [];
   for (let i = 0; i < Array.length; i++) {
     let currValue = Array[i];
     if (currValue % 2 === 1) {
-      result.push(currValue);
+      evenresult.push(currValue);
+    }
+    if (currValue % 2 === 0) {
+      oddresult.push(currValue);
     }
   }
-  return result;
+  return [oddresult, evenresult];
 }
