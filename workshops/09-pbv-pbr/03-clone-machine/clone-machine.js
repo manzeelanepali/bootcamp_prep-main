79480d53924f163c504380bbdf966ed1354cb408
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-13 15:14:28 +0545

    clone machine

--- workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
+++ workshops/09-pbv-pbr/03-clone-machine/clone-machine.js
@@ -1,1 +1,13 @@
 // YOUR CODE BELOW
+function cloneMachine(object) {
+  let finalResult = {};
+
+  for (let key in object) {
+    finalResult[key] = object[key];
+  }
+  finalResult.name += "Clone";
+  finalResult.offspring = [];
+  object.offspring.push(finalResult.name);
+
+  return finalResult;
+}
