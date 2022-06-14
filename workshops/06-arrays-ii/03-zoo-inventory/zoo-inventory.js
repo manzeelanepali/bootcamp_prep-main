38f5a724cccbc8089b47bceb9ad9e29b441b0120
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-14 13:53:11 +0545

    zoo inventory

--- workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
+++ workshops/06-arrays-ii/03-zoo-inventory/zoo-inventory.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function zooInventory(array) {
+  result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    let currValue = array[i];
+    let name = currValue[0];
+    let type = currValue[1][0];
+    let age = currValue[1][1];
+    sentence = `${name} the ${type} is ${age}.`;
+
+    result.push(sentence);
+  }
+
+  return result;
+}
