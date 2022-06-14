780fb8b21906cd8ebb3b547ee99f9d101ecabb2f
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-14 16:23:39 +0545

    2 join array

--- workshops/06-arrays-ii/06-my-join/my-join.js
+++ workshops/06-arrays-ii/06-my-join/my-join.js
@@ -1,1 +1,17 @@
 // YOUR CODE BELOW
+
+function myJoin(array, separator = ",") {
+  result = "";
+
+  for (let i = 0; i < array.length - 1; i++) {
+    currValue = array[i];
+
+    if (currValue === null || currValue === undefined) {
+      currValue = "";
+    }
+
+    result = result + currValue + separator;
+  }
+
+  return result + array[array.length - 1];
+}
