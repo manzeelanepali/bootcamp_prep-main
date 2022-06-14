34d573687ccff3a016a3cdcf514247c7eca215a5
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-14 09:50:01 +0545

    reverse array

--- workshops/09-pbv-pbr/04-my-splice/my-splice.js
+++ workshops/09-pbv-pbr/04-my-splice/my-splice.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+// let myArray = [1, 2, 3];
+
+// mySplice(myArray, 1, 1, 'apples')) // => [2]
+
+// console.log(myArray)    // [1,'apples', 3]
+
+function mySplice(array) {
+  let result = [];
+  for (i = 0; i < array.length; i++) {
+    currValue = array[i];
+  }
+
+  return result;
+}
--- workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
+++ workshops/09-pbv-pbr/05-reverse-array/reverse-array.js
@@ -1,1 +1,16 @@
 // YOUR CODE BELOW
+// let myArray = [1, 2, 3, 4];
+// reverse(myArray);
+// console.log(myArray) // [4, 3, 2, 1]
+function reverseArray(array) {
+  let result = [];
+  for (let i = array.length - 1; i >= 0; i--) {
+    currValue = array[i];
+    result.push(currValue);
+  }
+  for (let i = 0; result.length > i; i++) {
+    array[i] = result[i];
+  }
+
+  return array;
+}
