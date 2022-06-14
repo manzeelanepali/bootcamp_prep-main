fed228dad1a2ebd4e13f6093b0522bb485e0478e
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-14 14:37:29 +0545

    make grid

--- workshops/06-arrays-ii/04-make-grid/make-grid.js
+++ workshops/06-arrays-ii/04-make-grid/make-grid.js
@@ -1,1 +1,21 @@
 // YOUR CODE BELOW
+//makeGrid(3,4);
+/* => [[1, 2, 3],
+       [1, 2, 3],
+       [1, 2, 3],
+       [1, 2, 3]]
+*/
+
+function makeGrid(columns, rows) {
+  result = [];
+  for (let i = 0; i < rows; i++) {
+    let currResult = [];
+
+    for (let j = 0; j < columns; j++) {
+      currResult.push(j + 1);
+    }
+
+    result.push(currResult);
+  }
+  return result;
+}
