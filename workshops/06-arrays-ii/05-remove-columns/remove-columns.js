cf08054333609711ccd9621359413f94fe7b56e8
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-14 15:23:41 +0545

    remove columns

--- workshops/06-arrays-ii/05-remove-columns/remove-columns.js
+++ workshops/06-arrays-ii/05-remove-columns/remove-columns.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function removeColumns(originalGrid, numColums) {
+  let result = [];
+
+  for (let i = 0; i < originalGrid.length; i++) {
+    let currValue = originalGrid[i];
+    let innerColoumn = [];
+
+    for (let j = 0; j < currValue.length - numColums; j++) {
+      innerColoumn.push(currValue[j]);
+    }
+    result.push(innerColoumn);
+  }
+  return result;
+}
