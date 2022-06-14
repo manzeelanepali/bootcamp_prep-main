ac9775a98eaf3e2b5d7b6ba9d259528dcea7e8d0
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-14 10:59:10 +0545

    deeper copy

--- workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
+++ workshops/09-pbv-pbr/06-deeper-copy/deeper-copy.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+
+function deeperCopy(array) {
+  result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    currValue = array[i];
+
+    if (Array.isArray(currValue)) {
+      let newArray = [];
+      for (j = 0; j < currValue.length; j++) {
+        newArray.push(currValue[j]);
+      }
+      result.push(newArray);
+    } else {
+      result.push(currValue);
+    }
+  }
+  return result;
+}
