24fbf6d2d1ae9ebe9a2edb6ba8ad279bb7725d16
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-15 13:04:20 +0545

    finder function

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,8 +1,8 @@
 // YOUR CODE BELOW
 function finderFunction(array, callback) {
-  result = -1;
+  let result = -1;
   for (let i = 0; i < array.length; i++) {
-    currValue = array[i];
+    let currValue = array[i];
     let val = callback(currValue);
     if (val) {
       return i;
