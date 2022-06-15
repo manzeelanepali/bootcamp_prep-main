3e3d723a7c302b09a4b0f5b84451e99b35afed16
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-15 13:01:05 +0545

    finder function

--- workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
+++ workshops/10-higher-order-functions-i/02-finder-function/finder-function.js
@@ -1,1 +1,12 @@
 // YOUR CODE BELOW
+function finderFunction(array, callback) {
+  result = -1;
+  for (let i = 0; i < array.length; i++) {
+    currValue = array[i];
+    let val = callback(currValue);
+    if (val) {
+      return i;
+    }
+  }
+  return result;
+}
