2457452749f342fa66cee2024e64fbd90367bcac
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-15 12:30:02 +0545

    function Logger

--- workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
+++ workshops/10-higher-order-functions-i/01-function-logger/function-logger.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+
+function functionLogger(func, arg) {
+  console.log("Function starting ");
+
+  let result = func(arg);
+
+  console.log("Function complete");
+
+  return result;
+}
