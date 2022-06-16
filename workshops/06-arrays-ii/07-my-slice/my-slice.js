974fddd95ed4bbdd9c0a0cae668c9f691759313a
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-16 13:27:25 +0545

    my slice array 2

--- workshops/06-arrays-ii/07-my-slice/my-slice.js
+++ workshops/06-arrays-ii/07-my-slice/my-slice.js
@@ -1,1 +1,20 @@
 // YOUR CODE BELOW
+// mySlice([1, 2, 3], 1) // => [2, 3]
+// mySlice([1, 2, 3], 1, 2) // => [2]
+// ([1, 2, 3]) // => [1, 2, 3]
+function mySlice(array, startIdx = 0, EndIdx = array.length) {
+  let result = [];
+  if (startIdx < 0) {
+    startIdx = startIdx + array.length;
+  }
+  if (EndIdx < 0) {
+    EndIdx = EndIdx + array.length;
+  }
+  for (let i = startIdx; i < EndIdx; i++) {
+    let currVal = array[i];
+
+    result.push(currVal);
+  }
+
+  return result;
+}
--- workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
+++ workshops/10-higher-order-functions-i/03-whos-a-special/whos-a-special.js
@@ -1,1 +1,6 @@
 // YOUR CODE BELOW
+function whosASpecial(array) {
+  let result = "";
+
+  return result;
+}
