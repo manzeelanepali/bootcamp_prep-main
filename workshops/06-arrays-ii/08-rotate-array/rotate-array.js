cb7fbe8e3d06f3c9997bd30247a5d451d86590f9
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-16 14:31:09 +0545

    rotate array

--- workshops/06-arrays-ii/08-rotate-array/rotate-array.js
+++ workshops/06-arrays-ii/08-rotate-array/rotate-array.js
@@ -1,1 +1,7 @@
 // YOUR CODE BELOW
+function rotateArray(originalArray, rotateNum) {
+  let front = originalArray.slice(-rotateNum);
+  let end = originalArray.slice(0, -rotateNum);
+
+  return front.concat(end);
+}
