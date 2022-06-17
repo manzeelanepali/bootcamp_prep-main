a2d591c5761825ea8fe20647287c5223e207b957
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-17 16:52:47 +0545

    call Them all object method

--- workshops/08-object-methods/01-say-my-name/say-my-name.js
+++ workshops/08-object-methods/01-say-my-name/say-my-name.js
@@ -3,7 +3,7 @@ let me = {
   name: "kate",
   getGreeting: function () {
     // return `hi, my name is ${this.name}`;
-    return "Hi" + " " + "my name is " + this.name + ".";
+    return "Hi," + " " + "my name is " + this.name + ".";
   },
 };
 console.log(me.getGreeting("kate"));
--- workshops/08-object-methods/03-call-them-all/call-them-all.js
+++ workshops/08-object-methods/03-call-them-all/call-them-all.js
@@ -1,1 +1,11 @@
 // YOUR CODE BELOW
+function callThemAll(obj, value) {
+  let result = [];
+  for (let key in obj) {
+    let currVal = obj[key];
+    if (typeof currVal === "function") {
+      result.push(currVal(value));
+    }
+  }
+  return result;
+}
