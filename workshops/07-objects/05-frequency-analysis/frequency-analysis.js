51ee23fd0508e88a86a05ac978a4e0a93659b9e7
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-17 13:25:05 +0545

    frequency analysisis object

--- workshops/07-objects/05-frequency-analysis/frequency-analysis.js
+++ workshops/07-objects/05-frequency-analysis/frequency-analysis.js
@@ -1,1 +1,15 @@
 // YOUR CODE BELOW
+function frequencyAnalysis(string) {
+  result = {};
+
+  for (let i = 0; i < string.length; i++) {
+    let currValue = string[i].toLowerCase();
+    if (currValue in result) {
+      result[currValue]++;
+    } else {
+      result[currValue] = 1;
+    }
+  }
+
+  return result;
+}
