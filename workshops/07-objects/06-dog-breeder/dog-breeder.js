d2cd25080315c5a9bacd1d24424b35f62214de0f
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-17 14:33:49 +0545

    dog breeder

--- workshops/07-objects/06-dog-breeder/dog-breeder.js
+++ workshops/07-objects/06-dog-breeder/dog-breeder.js
@@ -1,1 +1,14 @@
 // YOUR CODE BELOW
+// dogBreeder('Sam', 12)    // => {name: 'Sam', age: 12}
+
+// dogBreeder(15)    // => {name:'Steve', age: 15}
+
+function dogBreeder(name = "Steve", age = 0) {
+  let newDog = { name, age };
+  if (typeof name === "number") {
+    newDog.age = name;
+    newDog.name = "Steve";
+  }
+
+  return newDog;
+}
