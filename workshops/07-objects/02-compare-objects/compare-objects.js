ea01055703b6ae1bd38a73fcb0b2ca4abf75c614
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-17 10:09:12 +0545

    leetTransalator object 3

--- workshops/07-objects/02-compare-objects/compare-objects.js
+++ workshops/07-objects/02-compare-objects/compare-objects.js
@@ -3,12 +3,12 @@
 function compareObjects(object1, object2) {
   let keys1 = Object.keys(object1); // object.keys converts the values in the object1 to the array value
   let keys2 = Object.keys(object2);
-  if (keys1.length === keys2.length) {
-    return true;
+  if (keys1.length !== keys2.length) {
+    return false;
   }
   for (let key of keys1) {
-    if (object1[key] === object2[key]) {
-      return true;
+    if (object1[key] !== object2[key]) {
+      return false;
     }
   }
   return true;
--- workshops/07-objects/03-leet-translator/leet-translator.js
+++ workshops/07-objects/03-leet-translator/leet-translator.js
@@ -1,4 +1,77 @@
-let letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-let leetChars = ['@', '8', '(', '|)', '3', 'ph', 'g', '#','l', '_|', '|<', '1', "|'|'|", '/\/', '0', '|D', '(,)', '|2', '5', '+', '|_|', '|/', "|/|/'",'><', 'j', '2'];
+let letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let leetChars = [
+  "@",
+  "8",
+  "(",
+  "|)",
+  "3",
+  "ph",
+  "g",
+  "#",
+  "l",
+  "_|",
+  "|<",
+  "1",
+  "|'|'|",
+  "//",
+  "0",
+  "|D",
+  "(,)",
+  "|2",
+  "5",
+  "+",
+  "|_|",
+  "|/",
+  "|/|/'",
+  "><",
+  "j",
+  "2",
+];
 
 // YOUR CODE BELOW
+
+function leetTranslator(str) {
+  let result = "";
+  let leetCodex = {};
+  console.log(letters.length);
+
+  for (let i = 0; i < letters.length; i++) {
+    let currValue = letters[i];
+    let leetcode = leetChars[i];
+    leetcode[currValue] = leetcode;
+  }
+  for (let i = 0; i < str.length; i++) {
+    let currletters = str[i].toLowerCase();
+    let currentVal = leetCodex[currletters];
+    result += currentVal;
+  }
+  return result;
+}
+console.log(leetTranslator("fullstack"));
