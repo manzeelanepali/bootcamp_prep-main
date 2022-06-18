a4964ec1229ed1b105ae0b4de4669fc2e7473230
manzeelanepali <nepalimanzeela@gmail.com>
2022-06-18 11:06:20 +0545

    tic tac toe problems

--- workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
+++ workshops/08-object-methods/04-tic-tac-toe/tic-tac-toe.js
@@ -1,1 +1,23 @@
 // YOUR CODE BELOW
+
+let ticTacToe = {
+  board: [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ],
+  move: function (char, row, column) {
+    if (this.board[row][column] === null) {
+      this.board[row][column] = char;
+    }
+    return this.board;
+  },
+  clear: function () {
+    this.board = [
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ];
+    return this.board;
+  },
+};
